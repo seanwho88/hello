43e14557e887d0f68e44a53033f2b648a2977cac
Linh B Ngo <lngo@wcupa.edu>
2022-09-20 17:43:54 -0400

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-   printf("Hello, Fall 2022 with Kubernetes!");
+   printf("Hello, Fall 2022 with Kubernetes!!!");
    printf("There and commit again");
    return 0;
 }
